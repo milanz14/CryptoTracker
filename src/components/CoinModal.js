6d5907486960c9e8dfe6c3af47befbdba7ad6a4a
Milan Zagorac <zagoracm@gmail.com>
2023-02-18 09:38:36 -0500

    linting

--- src/components/CoinModal.js
+++ src/components/CoinModal.js
@@ -2,41 +2,31 @@ import React from "react";
 import "../styles/CoinModal.css";
 
 const CoinModal = (props) => {
-    const {
-        currentPrice,
-        high24h,
-        low24h,
-        priceChange,
-        name,
-        image,
-        showModal,
-    } = props;
+  const { currentPrice, high24h, low24h, priceChange, name, image, showModal } =
+    props;
 
-    return (
-        <div className="modalBackground">
-            <div className="modalContainer">
-                <button
-                    className="titleCloseBtn"
-                    onClick={() => showModal(false)}
-                >
-                    X
-                </button>
-                <div clasName="modalTitle">
-                    <h1>{name}</h1>
-                </div>
-                <div clasName="modalBody">
-                    <img src={image} />
-                    <p>{currentPrice}</p>
-                    <p>{high24h}</p>
-                    <p>{low24h}</p>
-                    <p>{priceChange}</p>
-                </div>
-                <div clasName="modalFooter">
-                    <button onClick={() => showModal(false)}>Close</button>
-                </div>
-            </div>
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <button className="titleCloseBtn" onClick={() => showModal(false)}>
+          X
+        </button>
+        <div clasName="modalTitle">
+          <h1>{name}</h1>
         </div>
-    );
+        <div clasName="modalBody">
+          <img src={image} />
+          <p>{currentPrice}</p>
+          <p>{high24h}</p>
+          <p>{low24h}</p>
+          <p>{priceChange}</p>
+        </div>
+        <div clasName="modalFooter">
+          <button onClick={() => showModal(false)}>Close</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CoinModal;
