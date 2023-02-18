7812e00a1fb5365901e58d5ca16571edbc9a17c1
Milan Zagorac <zagoracm@gmail.com>
2023-02-18 11:06:34 -0500

    update packages, create simple grid instead of list

--- src/components/FetchComponent.js
+++ src/components/FetchComponent.js
@@ -3,7 +3,17 @@ import axios from "axios";
 import CoinList from "./CoinList";
 import "../styles/FetchComponent.css";
 import SearchFilter from "./SearchFilter";
-import { UnorderedList, ListItem } from "@chakra-ui/react";
+import {
+  //   UnorderedList,
+  //   ListItem,
+  SimpleGrid,
+  Card,
+  CardHeader,
+  CardBody,
+  Text,
+  CardFooter,
+  Button,
+} from "@chakra-ui/react";
 const BASE_URL =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 // const BASE_URL = 'https://api.coingecko.com/api/v3/'
@@ -40,7 +50,7 @@ const FetchComponent = () => {
       <h2>CryptoTracker</h2>
       <div>{!coins && <h2>No Coins Found </h2>}</div>
       <SearchFilter filterCoins={filterCoins} />
-      <UnorderedList>
+      {/* <UnorderedList>
         {coins.map((coin) => {
           return (
             <ListItem key={coin.id}>
@@ -59,7 +69,20 @@ const FetchComponent = () => {
             </ListItem>
           );
         })}
-      </UnorderedList>
+      </UnorderedList> */}
+      <SimpleGrid>
+        {coins.map((coin) => (
+          <Card key={coin.id}>
+            <CardHeader>{coin.name}</CardHeader>
+            <CardBody>
+              <Text>{coin.symbol}</Text>
+            </CardBody>
+            <CardFooter>
+              <Button>View</Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </SimpleGrid>
     </div>
   );
 };
