b012b745c001754c1f77ee1adb976aef438a40bd
Milan Zagorac <zagoracm@gmail.com>
2023-02-20 12:59:36 -0500

    set up for chart - dep not working properly

--- src/components/ChartComponent.js
+++ src/components/ChartComponent.js
@@ -1,15 +1,4 @@
 import { useState } from "react";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
 
 const ChartComponent = (props) => {
   const { coinPrices } = props;
