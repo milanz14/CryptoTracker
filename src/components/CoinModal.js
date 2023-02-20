0ac9f17bf954da91bc35db7520338cb5dc193698
Milan Zagorac <zagoracm@gmail.com>
2023-02-19 19:02:51 -0500

    temp remove spinner from modal:

--- src/components/CoinModal.js
+++ src/components/CoinModal.js
@@ -9,29 +9,22 @@ const CoinModal = (props) => {
   const { id, name, setIsModalShowing } = props;
 
   const [coinPrices, setCoinPrices] = useState(null);
-  const [isDataLoaded, setIsDataLoaded] = useState(false);
 
   return (
     <>
-      {isDataLoaded ? (
-        <>
-          <div className="modal-container">
-            {/* <ChartComponent priceData={coinPrices} /> */}
-            <button
-              className="title-close-btn"
-              onClick={() => setIsModalShowing((prev) => !prev)}>
-              &times;
-            </button>
-            <div className="modal-title">
-              <h1>{name}</h1>
-            </div>
-            <div className="modal-body">lorem80</div>
-          </div>
-          <div className="modal-overlay"></div>
-        </>
-      ) : (
-        <Spinner />
-      )}
+      <div className="modal-container">
+        {/* <ChartComponent priceData={coinPrices} /> */}
+        <button
+          className="title-close-btn"
+          onClick={() => setIsModalShowing((prev) => !prev)}>
+          &times;
+        </button>
+        <div className="modal-title">
+          <h1>{name}</h1>
+        </div>
+        <div className="modal-body">lorem80</div>
+      </div>
+      <div className="modal-overlay"></div>
     </>
   );
 };
